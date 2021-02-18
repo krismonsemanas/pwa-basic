e15f930677c7ede02c4f7476b49675db20d35920
krismon setiawan <krismonsetiawan57@gmail.com>
2021-02-19 00:23:59 +0700

    Event fetch pwa

--- sw.js
+++ sw.js
@@ -7,3 +7,8 @@ self.addEventListener('install', e => {
 self.addEventListener('activate', e => {
     console.log('sw di aktifkan')
 })
+
+// fetch data
+self.addEventListener('fetch' , e => {
+    console.log('event fetch', e)
+})
