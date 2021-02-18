c268b4c3c432eaa39bda550aac38d00322070194
krismon setiawan <krismonsetiawan57@gmail.com>
2021-02-19 01:42:57 +0700

    Limit dynamic cache

--- sw.js
+++ sw.js
@@ -7,8 +7,19 @@ const assets = [
     '/asset/js/ui.js',
     '/asset/materialize/js/materialize.min.js',
     '/asset/materialize/css/materialize.min.css',
-    'https://fonts.googleapis.com/icon?family=Material+Icons'
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+    '/pages/fallback.html'
 ]
+// limit number cache
+const limitNumCahce = (cacheName, num) => {
+    caches.open(cacheName).then(cache => {
+        cache.keys().then(keys => {
+            if (keys.length > num ) {
+                cache.delete(keys[0]).then(limitNumCahce(cacheName, num))
+            }
+        })
+    })
+}
 // install
 self.addEventListener('install', e => {
     e.waitUntil(
@@ -30,10 +41,11 @@ self.addEventListener('fetch' , e => {
             return staticRes || fetch(e.request).then(dynamicRes => {
                 return caches.open(dynamicCache).then(cache => {
                     cache.put(e.request.url, dynamicRes.clone())
-                        return dynamicRes
+                    limitNumCahce(dynamicCache, 2)
+                    return dynamicRes
                 })
             })
-        })
+        }).catch(() => caches.match('/pages/fallback.html'))
     )
 })
 
